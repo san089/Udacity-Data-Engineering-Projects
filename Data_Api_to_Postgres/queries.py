cf4c547967a741a8ca764d2aa13a8d0c2c4f9fd7
san089 <sanchitkumar013@gmail.com>
2020-03-04 23:18:42 -0500

    Starting project

---
+++ Data_Api_to_Postgres/queries.py
@@ -0,0 +1,35 @@
+create_business_schema = """CREATE SCHEMA IF NOT EXISTS yelp;"""
+
+create_business_table = """
+CREATE TABLE IF NOT EXISTS yelp.business (
+	business_id varchar,
+	business_name varchar,
+	image_url varchar,
+	url varchar,
+	review_count int, 
+	categories varchar,
+	rating float,
+	latitude float,
+	longitude float,
+	price varchar,
+	location varchar,
+	phone varchar
+);
+"""
+
+insert_business_table = """INSERT INTO yelp.business VALUES ('{}', '{}', '{}', '{}', {}, '{}', {}, {}, {}, '{}', '{}', '{}')
+                        ON CONFLICT (business_id)
+                        DO UPDATE SET
+                        business_id = EXCLUDED.business_id,
+                        business_name = EXCLUDED.business_name,
+                        image_url = EXCLUDED.image_url,
+                        url = EXCLUDED.url,
+                        review_count = EXCLUDED.review_count,
+                        categories = EXCLUDED.categories,
+                        rating = EXCLUDED.rating,
+                        latitude = EXCLUDED.latitude,
+                        longitude = EXCLUDED.longitude,
+                        price = EXCLUDED.price,
+                        location = EXCLUDED.location,
+                        phone = EXCLUDED.phone;
+                        """
